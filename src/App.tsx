7b824f4f776fe133850ccf07f6512fdfdd01bef1
diegobarata <porcocego177@gmail.com>
2021-09-25 14:34:00 -0300

    frontend 1%

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <h1>InstaCat</h1>
+
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
